0089336406f90a3a29992c5aa3a654556b9c1186
Alice Daubree <alice.daubree@yahoo.fr>
2022-09-20 11:44:03 +0200

    authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "car#home"
+  get "profile", to: "pages#profil"
+  resources :car do
+    resources :booking
+    # , only: [ :create,:destroy ]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
